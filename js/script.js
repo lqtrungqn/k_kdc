4130257b2111aeed61d1cf5185c41b463b029bc8
vdbaozn <108704763+vdbaozn@users.noreply.github.com>
2023-05-12 09:17:14 +0700

    bao up

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+    // back top
+    $(function () {
+        var topBtn = $('#page_top');
+        topBtn.hide();
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                topBtn.fadeIn();
+            } else {
+                topBtn.fadeOut();
+            }
+        });
+        topBtn.click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 600);
+            return false;
+        });
+    });
+
+
+    // tel sp
+    var ua = navigator.userAgent.toLowerCase();
+    var isMobile = /iphone/.test(ua) || /android(.+)?mobile/.test(ua);
+
+    if (!isMobile) {
+        $('a[href^="tel:"]').on('click', function (e) {
+            e.preventDefault();
+        });
+    }
+    
+    AOS.init({
+        disable: 'mobile',
+        once: true,
+    });
+
+    
+});
